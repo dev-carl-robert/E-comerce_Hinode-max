ef2420dc863e7a975a87bc55499c52c4d36175d9
Carlos Robert <149803558+dev-carl-robert@users.noreply.github.com>
2023-12-05 07:38:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".catalogo", {
+  spaceBetween: 30,
+  slidesPerView: 3,
+  centeredSlides: true,
+  fade: true,
+  autoplay: {
+    delay: 2200,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  keyboard: true,
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
+
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
